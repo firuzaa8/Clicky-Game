752ad7470a3ff7453170d17da7bf795d66ae7f69
Firuza Abramyan <firuzaa8@gmail.com>
2020-02-29 14:48:28 -0500

    Updates

---
+++ precache-manifest.be37e6cd908c60cd704d25ab7c6385c6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5071a63b2d51bd93651cc444eca4293",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "684d9e815f924076beb2",
+    "url": "/Clicky-Game/static/css/main.da770de2.chunk.css"
+  },
+  {
+    "revision": "81c9b41b1269bbb4650a",
+    "url": "/Clicky-Game/static/js/2.4f6d5cb4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Clicky-Game/static/js/2.4f6d5cb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "684d9e815f924076beb2",
+    "url": "/Clicky-Game/static/js/main.032ccd2f.chunk.js"
+  },
+  {
+    "revision": "a2d6f74684f66c844de8",
+    "url": "/Clicky-Game/static/js/runtime-main.05fb1c39.js"
+  },
+  {
+    "revision": "4c5c3bb86e7c3795bd4aaa3ccc8fcaf8",
+    "url": "/Clicky-Game/static/media/bruce.4c5c3bb8.jpeg"
+  },
+  {
+    "revision": "1b5509b5f7ab015115bf9df5a2691b26",
+    "url": "/Clicky-Game/static/media/chris.1b5509b5.jpg"
+  },
+  {
+    "revision": "881318a97e7f508d3db98bad894dc410",
+    "url": "/Clicky-Game/static/media/consuela.881318a9.png"
+  },
+  {
+    "revision": "5ebdaf86722926faef1a89d2103f0e4c",
+    "url": "/Clicky-Game/static/media/mort.5ebdaf86.jpg"
+  },
+  {
+    "revision": "e5102907752b7d94e7a5c40e2622fe64",
+    "url": "/Clicky-Game/static/media/peter.e5102907.jpeg"
+  },
+  {
+    "revision": "da972d44e92f0f6891fa12a320a516e0",
+    "url": "/Clicky-Game/static/media/quagmire.da972d44.png"
+  },
+  {
+    "revision": "ce9eaadbf378de519f893dcb76ecb4f0",
+    "url": "/Clicky-Game/static/media/stewie.ce9eaadb.jpg"
+  }
+]);
